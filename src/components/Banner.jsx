518ca2380778369c9e13639ac52b70f305e51cef
bipinemp <bhandaribipin3.39@gmail.com>
2023-04-07 21:31:37 +0545

    some images for slider

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,51 +1,23 @@
 import React from "react";
-import banner1 from "../assets/banner1.webp";
-import banner2 from "../assets/banner2.jpg";
-import banner3 from "../assets/banner3.png";
-import banner4 from "../assets/banner4.jpg";
-import { useState } from "react";
-import { useEffect } from "react";
+import bann1 from "../assets/bann1.jpg";
+import bann2 from "../assets/bann2.jpg";
+import bann3 from "../assets/bann3.png";
+import Carousel from "./Carousel";
 
 function Banner() {
-  const [current, setCurrent] = useState(0);
-
-  const images = [
-    { img: banner1, desc: "Hello world !!" },
-    { img: banner2, desc: "Best framework reactjs" },
-    { img: banner3, desc: "Free deal available hurry up !!" },
-    { img: banner4, desc: "Grab Upto 50% Off " },
-  ];
-
-  const handlePrev = () => {
-    setCurrent((curr) =>
-      curr === images.length - 4 ? images.length - 1 : curr - 1
-    );
-  };
-  const handleNext = () => {
-    setCurrent((curr) => (curr === images.length - 1 ? 0 : curr + 1));
-  };
-
-  useEffect(() => {
-    console.log(current);
-  }, [current]);
+  const images = [bann1, bann2, bann3];
 
   return (
     <div className="banner">
-      {images.map((item, index) => {
-        return (
-          <div className="banneritem">
-            <div>
-              <h1>{item.desc}</h1>
-              <button>Buy Now</button>
-            </div>
-            <img src={item.img} alt="bannerimages" />
-          </div>
-        );
-      })}
-      <div className="buttons">
-        <button onClick={handlePrev}>prev</button>
-        <button onClick={handleNext}>next</button>
-      </div>
+      <Carousel autoSlide={true}>
+        {images.map((s) => (
+          <img
+            src={s}
+            alt="bannerimg"
+            style={{ width: "800px", objectFit: "cover" }}
+          />
+        ))}
+      </Carousel>
     </div>
   );
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+function Carousel({
+  children: slides,
+  autoSlide = false,
+  autoSlideInterval = 3000,
+}) {
+  const [current, setCurrent] = useState(0);
+  const prev = () => {
+    setCurrent((curr) => (curr === 0 ? slides.length - 1 : current - 1));
+  };
+  const next = () => {
+    setCurrent((curr) => (curr === slides.length - 1 ? 0 : current + 1));
+  };
+  useEffect(() => {
+    if (!autoSlide) return;
+    const slideInterval = setInterval(next, autoSlideInterval);
+    return () => clearInterval(slideInterval);
+  });
+
+  return (
+    <div style={{ overflow: "hidden", position: "relative" }}>
+      <div
+        style={{
+          Width: "800px",
+          display: "flex",
+          transition: "transform 500ms ease-out",
+          transform: `translateX(-${current * 100}%)`,
+        }}
+      >
+        {slides}
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          inset: "0",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <AiOutlineLeft
+          onClick={prev}
+          fontSize="50px"
+          color="var(--LightLightBlack)"
+          cursor="pointer"
+        />
+        <AiOutlineRight
+          onClick={next}
+          fontSize="50px"
+          color="var(--LightLightBlack)"
+          cursor="pointer"
+        />
+      </div>
+      <div style={{ position: "absolute", bottom: "10px", right: 0, left: 0 }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "4px",
+          }}
+        >
+          {slides.map((_, index) => (
+            <div
+              style={{
+                transition: "all 150ms linear",
+                width: "6px",
+                height: "6px",
+                borderRadius: "25px",
+                background: "white",
+                padding: `${current === index ? "6px" : "3px"}`,
+                opacity: `${current === index ? "1" : "0.5"}`,
+              }}
+              onClick={() => setCurrent(index)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
